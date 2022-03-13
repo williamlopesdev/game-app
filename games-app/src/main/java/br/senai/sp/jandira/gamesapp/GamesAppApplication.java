4817a9cef74f0d6cc9c76cf45cfdbfbfd3888c04
williamlopesdev <wilcypherr@gmail.com>
2022-03-13 10:04:49 -0300

    primeiro commit

---
+++ games-app/src/main/java/br/senai/sp/jandira/gamesapp/GamesAppApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.gamesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesAppApplication.class, args);
+	}
+
+}
---
+++ games-app/src/test/java/br/senai/sp/jandira/gamesapp/GamesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.gamesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
