18cbf537f273678726d7c9f73bac3523b8efd5d4
williamlopesdev <wilcypherr@gmail.com>
2022-03-13 11:47:41 -0300

    criando coneção com banco mysql e primeiros atibutos da classe Game

---
+++ games-app/src/main/java/br/senai/sp/jandira/gamesapp/model/Game.java
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.gamesapp.model;
+
+public class Game {
+	
+	private Long id;
+	private String titulo;
+	private String produtora;
+	private boolean zerado;
+	private int nota;
+	private String console;
+
+}
