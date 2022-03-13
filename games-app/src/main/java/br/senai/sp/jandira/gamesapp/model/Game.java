1c2f85be34cbd67e97adebbb473c9d1e4ca33b72
williamlopesdev <wilcypherr@gmail.com>
2022-03-13 11:51:03 -0300

    criando getters e setters da classe Game e tambem o equals e hashcode

--- games-app/src/main/java/br/senai/sp/jandira/gamesapp/model/Game.java
+++ games-app/src/main/java/br/senai/sp/jandira/gamesapp/model/Game.java
@@ -1,7 +1,9 @@
 package br.senai.sp.jandira.gamesapp.model;
 
+import java.util.Objects;
+
 public class Game {
-	
+
 	private Long id;
 	private String titulo;
 	private String produtora;
@@ -9,4 +11,73 @@ public class Game {
 	private int nota;
 	private String console;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public boolean isZerado() {
+		return zerado;
+	}
+
+	public void setZerado(boolean zerado) {
+		this.zerado = zerado;
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(console, id, nota, produtora, titulo, zerado);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(console, other.console) && Objects.equals(id, other.id) && nota == other.nota
+				&& Objects.equals(produtora, other.produtora) && Objects.equals(titulo, other.titulo)
+				&& zerado == other.zerado;
+	}
+
+	
+	
 }
