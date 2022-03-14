51de7383f50ab19ef678e4cda08dd7259e623318
williamlopesdev <wilcypherr@gmail.com>
2022-03-14 17:34:46 -0300

    metodo get e metodo get por id

--- games-app/src/main/java/br/senai/sp/jandira/gamesapp/resource/GameResource.java
+++ games-app/src/main/java/br/senai/sp/jandira/gamesapp/resource/GameResource.java
@@ -1,14 +1,17 @@
 package br.senai.sp.jandira.gamesapp.resource;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
 import br.senai.sp.jandira.gamesapp.model.Game;
 import br.senai.sp.jandira.gamesapp.repository.GameRepository;
 
@@ -24,6 +27,18 @@ public class GameResource {
 		return "Olá mundo";
 	}
 	
+	@GetMapping("/api/games")
+	public List<Game> listar(){
+		
+		return gameRepository.findAll();
+	}
+	
+	@GetMapping("/api/games/{id}")
+	public Game listarJogoPorID(@PathVariable Long id){
+		
+		return gameRepository.findById(id).get();
+	}
+	
 	@PostMapping("/api/games")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Game save(@RequestBody Game game) {
@@ -31,4 +46,6 @@ public class GameResource {
 		return gameRepository.save(game);
 		
 	}
+	
+	
 }
