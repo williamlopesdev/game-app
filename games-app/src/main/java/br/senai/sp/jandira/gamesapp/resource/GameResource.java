206dd280058a9469ea93877c719ca359c7fe4999
williamlopesdev <wilcypherr@gmail.com>
2022-03-15 11:37:58 -0300

    update

--- games-app/src/main/java/br/senai/sp/jandira/gamesapp/resource/GameResource.java
+++ games-app/src/main/java/br/senai/sp/jandira/gamesapp/resource/GameResource.java
@@ -1,12 +1,14 @@
 package br.senai.sp.jandira.gamesapp.resource;
 
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,6 +54,13 @@ public class GameResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void excluir(@PathVariable Long id) {
 		gameRepository.deleteById(id);
+	}
+	
+	@PutMapping("/api/games/{id}")
+	public Game atualizar(@PathVariable Long id, @RequestBody Game game) {
+		Game gameAtual = gameRepository.findById(id).get();
+		BeanUtils.copyProperties(game, gameAtual, "id");
+		return gameRepository.save(gameAtual);
 	}
 	
 	
